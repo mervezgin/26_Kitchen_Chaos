60bdf4ded92b776793119d72083f1f479852ab63
mervezgin <mervezgin@gmail.com>
2025-04-04 11:10:58 +0300

    -[ADD]player, player controller script, movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float moveSpeed = 7f;
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0f, 0f);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1f;
+        }
+        inputVector = inputVector.normalized;
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+}
