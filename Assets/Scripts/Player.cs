a8bda0ea2ea37c9d66acd347378752242c5fd83c
mervezgin <mervezgin@gmail.com>
2025-04-10 11:32:45 +0300

    -[ADD]collision detection by capsule raycast, and player movement fixed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,12 +10,43 @@ public class Player : MonoBehaviour
     private float playerHeight = 2f;
     private void Update()
     {
-
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
 
         float moveDistance = moveSpeed * Time.deltaTime;
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+        if (!canMove)
+        {
+            //cannot move towards movedir
+
+            //attempt only x movement 
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove)
+            {
+                //can move only on the x 
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                //cannot move only on the x 
+
+                //attempt only z movement
+                Vector3 moveDirectionZ = new Vector3(0f, 0f, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+
+                if (canMove)
+                {
+                    //can move only on the z
+                    moveDirection = moveDirectionZ;
+                }
+                else
+                {
+                    //cannot move any directions
+                }
+            }
+        }
         if (canMove)
         {
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
